99a22fbef8fc78ec8c1a6bd58e1080bbaba1260d
danfeit <daniel.feitosa.mis@gmail.com>
2024-11-07 23:53:05 -0300

    CadastroNFOut.py

--- CadastroNFOut.py
+++ CadastroNFOut.py
@@ -133,9 +133,13 @@ def enviar_email(numero_nota, empresa_pagadora, valor_total, items, arquivos_ane
 
     # Salvar o Excel em um buffer de memória
     excel_buffer = BytesIO()
-    with pd.ExcelWriter(excel_buffer, engine='openpyxl') as writer:
-        df.to_excel(writer, index=False)
-    excel_buffer.seek(0)
+    df.to_excel(excel_buffer, index=False)
+    excel_buffer.seek(0)  # Retorna o ponteiro para o início do arquivo
+
+    #excel_buffer = BytesIO()
+    #with pd.ExcelWriter(excel_buffer, engine='openpyxl') as writer:
+        #df.to_excel(writer, index=False)
+    #excel_buffer.seek(0)
 
     # Nome do arquivo com o formato desejado
     nome_arquivo = f"notafiscal - {numero_nota}.xlsx"
